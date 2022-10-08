1f7746ede76f3039f57fdd183c756aee3b80e7c4
geyler <geylerps@gmail.com>
2022-10-08 17:24:51 -0300

    propellerads

--- pages/index.js
+++ pages/index.js
@@ -44,100 +44,149 @@ export default function Homex(props) {
   return (
     <div className="contenedor">
       <Head>
-      <meta name="viewport" content="width=device-width"/>
-      <link rel="icon" href="/favicon.ico" />
-      <title>Grupos para Unirse de { cat? cat : 'Whatsapp'} | Grupos de { cat? cat : 'Whatsapp'}</title>
-      <meta property="og:title" content={`Grupos para Unirse de ${ cat? cat : 'Whatsapp'} | Grupos de ${ cat? cat : 'Whatsapp'}`} />
-      <meta name="twitter:title" content={`Grupos para Unirse de ${ cat? cat : 'Whatsapp'} | Grupos de ${ cat? cat : 'Whatsapp'}`} />
-      <meta name="description" content={`¿Buscas Grupos de ${ cat? cat : 'Whatsapp'} para Unirte? En nuestra web GruposParaUnirse encontrarás una gran variedad de Grupos de ${ cat? cat : 'Whatsapp'} disponibles.`} />
-      <meta property="og:description" content={`¿Buscas Grupos de ${ cat? cat : 'Whatsapp'} para Unirte? En nuestra web GruposParaUnirse encontrarás una gran variedad de Grupos de ${ cat? cat : 'Whatsapp'} disponibles.`} />
-      <meta name="twitter:description" content={`¿Buscas Grupos de ${ cat? cat : 'Whatsapp'} para Unirte? En nuestra web GruposParaUnirse encontrarás una gran variedad de Grupos de ${ cat? cat : 'Whatsapp'} disponibles.`}/>
-      <meta name="keywords" content={` ${cat?cat:'Whatsapp'},Grupos de ${cat?cat:'Whatsapp'},Unirme a Grupos de  ${cat?cat:'Whatsapp'}, Link de Grupos de  ${cat?cat:'Whatsapp'}`} />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="robots" content="max-image-preview:large" />
-      <meta property="image" content={`https://gruposparaunirse.net/${rsocial}.jpg`} />
-      <meta property="og:image" content={`https://gruposparaunirse.net/${rsocial}.jpg`} />
-      <meta name="twitter:image" content={`https://gruposparaunirse.net/${rsocial}.jpg`} />
-      <link href={`https://gruposparaunirse.net/${rsocial}.jpg`} rel="image_src"/>
-      <meta property="image:width" content="1280" />
-      <meta property="image:height" content="720" />
-      <meta property="og:image:width" content="1280" />
-      <meta property="og:image:height" content="720" />
-      <meta name="robots" content="index,follow"/>
-      <meta property="og:locale" content="es_ES" />
-      <meta property="og:type" content="WebPage" />
-      <meta property="og:site_name" content="Grupos Para Unirse" />
-      <meta property="url" content={`https://gruposparaunirse.net${props.url}`} />
-      <meta property="og:url" content={`https://gruposparaunirse.net${props.url}`} />
-      <link rel="canonical" href={`https://gruposparaunirse.net${props.url}`} />
-      <meta name="msapplication-TileColor" content="#242424"/>
-      <meta name="msapplication-TileImage" content={`https://gruposparaunirse.net/favicon.png`}/>
+        <meta name="viewport" content="width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>
+          Grupos para Unirse de {cat ? cat : "Whatsapp"} | Grupos de{" "}
+          {cat ? cat : "Whatsapp"}
+        </title>
+        <meta name="propeller" content="7820ac37fe98bef7acd6d37121ee352f"/>
+        <meta
+          property="og:title"
+          content={`Grupos para Unirse de ${
+            cat ? cat : "Whatsapp"
+          } | Grupos de ${cat ? cat : "Whatsapp"}`}
+        />
+        <meta
+          name="twitter:title"
+          content={`Grupos para Unirse de ${
+            cat ? cat : "Whatsapp"
+          } | Grupos de ${cat ? cat : "Whatsapp"}`}
+        />
+        <meta
+          name="description"
+          content={`¿Buscas Grupos de ${
+            cat ? cat : "Whatsapp"
+          } para Unirte? En nuestra web GruposParaUnirse encontrarás una gran variedad de Grupos de ${
+            cat ? cat : "Whatsapp"
+          } disponibles.`}
+        />
+        <meta
+          property="og:description"
+          content={`¿Buscas Grupos de ${
+            cat ? cat : "Whatsapp"
+          } para Unirte? En nuestra web GruposParaUnirse encontrarás una gran variedad de Grupos de ${
+            cat ? cat : "Whatsapp"
+          } disponibles.`}
+        />
+        <meta
+          name="twitter:description"
+          content={`¿Buscas Grupos de ${
+            cat ? cat : "Whatsapp"
+          } para Unirte? En nuestra web GruposParaUnirse encontrarás una gran variedad de Grupos de ${
+            cat ? cat : "Whatsapp"
+          } disponibles.`}
+        />
+        <meta
+          name="keywords"
+          content={` ${cat ? cat : "Whatsapp"},Grupos de ${
+            cat ? cat : "Whatsapp"
+          },Unirme a Grupos de  ${cat ? cat : "Whatsapp"}, Link de Grupos de  ${
+            cat ? cat : "Whatsapp"
+          }`}
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="robots" content="max-image-preview:large" />
+        <meta
+          property="image"
+          content={`https://gruposparaunirse.net/${rsocial}.jpg`}
+        />
+        <meta
+          property="og:image"
+          content={`https://gruposparaunirse.net/${rsocial}.jpg`}
+        />
+        <meta
+          name="twitter:image"
+          content={`https://gruposparaunirse.net/${rsocial}.jpg`}
+        />
+        <link
+          href={`https://gruposparaunirse.net/${rsocial}.jpg`}
+          rel="image_src"
+        />
+        <meta property="image:width" content="1280" />
+        <meta property="image:height" content="720" />
+        <meta property="og:image:width" content="1280" />
+        <meta property="og:image:height" content="720" />
+        <meta name="robots" content="index,follow" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="WebPage" />
+        <meta property="og:site_name" content="Grupos Para Unirse" />
+        <meta
+          property="url"
+          content={`https://gruposparaunirse.net${props.url}`}
+        />
+        <meta
+          property="og:url"
+          content={`https://gruposparaunirse.net${props.url}`}
+        />
+        <link
+          rel="canonical"
+          href={`https://gruposparaunirse.net${props.url}`}
+        />
+        <meta name="msapplication-TileColor" content="#242424" />
+        <meta
+          name="msapplication-TileImage"
+          content={`https://gruposparaunirse.net/favicon.png`}
+        />
 
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2965450045901943"
-     crossOrigin="anonymous"></script>
-
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2965450045901943"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <ArticleJsonLd
-      url={`https://gruposparaunirse.net${props.url}`}
-      title={`Grupos para Unirse de ${ cat? cat : 'Whatsapp'} | Grupos de ${ cat? cat : 'Whatsapp'}`}
-      images={[
-        `https://gruposparaunirse.net/${rsocial}.jpg`,
-        `https://gruposparaunirse.net/favicon.png`,
-      ]}
-      authorName={[
-        {
-          name: "Grupos Para Unirse",
-          url:`https://gruposparaunirse.net/`
-        }
-      ]}
-      publisherName="Grupos Para Unirse"
-      publisherLogo={`https://gruposparaunirse.net/favicon.ico`}
-      description={`¿Buscas Grupos de ${ cat? cat : 'Whatsapp'} para Unirte? En nuestra web GruposParaUnirse encontrarás una gran variedad de Grupos de ${ cat? cat : 'Whatsapp'} disponibles.`}    
-    />
-    <BreadcrumbJsonLd
-      itemListElements={[
-        {
-          position: 1,
-          name: 'Telegram',
-          item: `https://gruposparaunirse.net/?r=Telegram`,
-        },
-        {
-          position: 2,
-          name: 'Whatsapp',
-          item: `https://gruposparaunirse.net/?r=Whatsapp`,
-        },
-        {
-          position: 3,
-          name: 'Facebook',
-          item: `https://gruposparaunirse.net/?r=Facebook`,
-        },
-        {
-          position: 4,
-          name: 'Reddit',
-          item: `https://gruposparaunirse.net/?r=Reddit`,
-        },
-        {
-          position: 5,
-          name: 'Discord',
-          item: `https://gruposparaunirse.net/?r=Discord`,
-        },
-        {
-          position: 6,
-          name: 'Signal',
-          item: `https://gruposparaunirse.net/?r=Signal`,
-        },
-        {
-          position: 7,
-          name: 'Viber',
-          item: `https://gruposparaunirse.net/?r=Viber`,
-        },
-      ]}
-    />
+        url={`https://gruposparaunirse.net${props.url}`}
+        title={`Grupos para Unirse de ${cat ? cat : "Whatsapp"} | Grupos de ${
+          cat ? cat : "Whatsapp"
+        }`}
+        images={[
+          `https://gruposparaunirse.net/${rsocial}.jpg`,
+          `https://gruposparaunirse.net/favicon.png`,
+        ]}
+        authorName={[
+          {
+            name: "Grupos Para Unirse",
+            url: `https://gruposparaunirse.net/`,
+          },
+        ]}
+        publisherName="Grupos Para Unirse"
+        publisherLogo={`https://gruposparaunirse.net/favicon.ico`}
+        description={`¿Buscas Grupos de ${
+          cat ? cat : "Whatsapp"
+        } para Unirte? En nuestra web GruposParaUnirse encontrarás una gran variedad de Grupos de ${
+          cat ? cat : "Whatsapp"
+        } disponibles.`}
+      />
+      <BreadcrumbJsonLd
+        itemListElements={[
+          {
+            position: 1,
+            name: "Telegram",
+            item: `https://gruposparaunirse.net/?r=Telegram`,
+          },
+          {
+            position: 2,
+            name: "Whatsapp",
+            item: `https://gruposparaunirse.net/?r=Whatsapp`,
+          },
+        ]}
+      />
 
       <div className="app">
         <Header perfil={props.session ? props.session.user : ""} />
-      
+
         <main className="main">
           <ul className="listado-inicio">
             <li className="li-buscador">
@@ -154,7 +203,11 @@ export default function Homex(props) {
             </li>
             <li className="titulo-home">
               {cat ? (
-                <h1>Grupos para unirte de {rSocial?rSocial:''}{rSocial && categ ?' / ':''}{categ?categ:''}</h1>
+                <h1>
+                  Grupos para unirte de {rSocial ? rSocial : ""}
+                  {rSocial && categ ? " / " : ""}
+                  {categ ? categ : ""}
+                </h1>
               ) : (
                 <h1>Grupos para unirte de Redes Sociales.</h1>
               )}
@@ -162,7 +215,7 @@ export default function Homex(props) {
 
             <li>
               {
-               // !cat && !pag ? <CardsCategorias/> : ''
+                // !cat && !pag ? <CardsCategorias/> : ''
               }
             </li>
 
@@ -204,12 +257,27 @@ export default function Homex(props) {
           />
 
           <div className="text-informacion-footer">
-          <br />
-          <br />
-          <Categorias red={rSocial} />
-          <br />
-          <br />
-          <TextosHome cat={cat}/>
+            <br />
+            <script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2965450045901943"
+              crossorigin="anonymous"
+            ></script>
+            <ins
+              class="adsbygoogle"
+              style="display:block"
+              data-ad-client="ca-pub-2965450045901943"
+              data-ad-slot="4247228277"
+              data-ad-format="auto"
+              data-full-width-responsive="true"
+            ></ins>
+            <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+
+            <br />
+            <Categorias red={rSocial} />
+            <br />
+            <br />
+            <TextosHome cat={cat} />
 
             <br />
             <br />
