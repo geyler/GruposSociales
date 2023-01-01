04350f43d956475e7013e7329bc073d285408ec5
geyler <geylerps@gmail.com>
2023-01-01 20:36:17 -0300

    popcash

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 function MyApp({ Component, pageProps }) {
-  return (<Component {...pageProps} /> )
+  return (<>
+  <Component {...pageProps} /> 
+  </>)
 }
 export default MyApp
