76e177b30331fd7e28097e35f87ea56977feab80
geyler <geylerps@gmail.com>
2023-01-01 20:46:00 -0300

    popcash

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,26 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React, { Fragment } from 'react';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head/>
+        <Head>
+          <Fragment>
+            <script async type="text/javascript">
+              var uid = '236265';
+              var wid = '672811';
+              var pop_tag = document.createElement('script');
+              pop_tag.src='//cdn.popcash.net/show.js';
+              document.body.appendChild(pop_tag);
+              pop_tag.onerror = function() {
+                pop_tag = document.createElement('script')
+                pop_tag.src='//cdn2.popcash.net/show.js';
+                document.body.appendChild(pop_tag);
+              }
+            </script>
+          </Fragment>
+        </Head>
         <body>
           <Main />
           <NextScript />
